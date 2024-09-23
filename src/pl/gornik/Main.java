696ac128ab9918fd1a5433069cfbb5fa631e0db9
mikkeyprogramista <mikkeyprogramista@gmail.com>
2024-09-23 11:32:29 +0200

    csac

---
+++ src/pl/gornik/Main.java
@@ -0,0 +1,13 @@
+package pl.gornik;
+
+public class Main {
+
+    public static double getCircumference(double r) {
+        double pi = 3.14;
+
+        return 2 * r * pi;
+    }
+    public static void main(String[] args) {
+        System.out.println(getCircumference(21.0));
+    }
+}
